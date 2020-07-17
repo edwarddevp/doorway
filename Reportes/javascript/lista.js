f154017600f67e37e1b6c803883e6d757047cc64
code.developer13 <dev13@codecraft.com>
2020-07-17 10:44:30 -0400

    Seccion de reportes terminada

--- Reportes/javascript/lista.js
+++ Reportes/javascript/lista.js
@@ -2,7 +2,7 @@ const dataTable = document.getElementById('dataTable');
 const dataTableContainer = document.getElementById('dataTableContainer');
 const tbody = document.getElementById('tbody');
 const loading = document.getElementById('loading');
-
+let table = '';
 
 const stopTableLoading = () => {
     dataTableContainer.classList.remove('display-none')
@@ -17,6 +17,10 @@ const startTableLoading = () => {
 const fillTable = async (initialDate,finalDate) => {
     startTableLoading()
 
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.lastChild);
+    }
+
     const res = await fetch('http://localhost:3000/report-list',{
         method: 'POST',
         body: JSON.stringify({initialDate,finalDate}), 
@@ -24,66 +28,58 @@ const fillTable = async (initialDate,finalDate) => {
           'Content-Type': 'application/json'
         }})
     const resJson = await res.json();
-    console.log(resJson)
 
-    resJson.map((cliente)=>{
+    
+
+    if(resJson.length > 0) {
+          resJson.map((item)=>{
+        const regex = /,/gi;
+        const nombreProductos = item.productosNombres.split(',')
+        const precioProductos = item.productosPrecios.split(',')
+        const productos = nombreProductos.map((nombre,index)=>nombre+' '+precioProductos[index]+'$\n').join().replace(regex,'')
+
+        const today = new Date(item.fecha);
+        const date = today.getFullYear() + '-' + (today.getMonth() +1) + '-' + today.getDate();
+
         const newtr = document.createElement("tr"); 
         const tdId = document.createElement("td"); 
-        tdId.innerHTML = cliente.id
+        tdId.innerHTML = item.id
         const tdName = document.createElement("td"); 
-        tdName.innerHTML = cliente.name
+        tdName.innerHTML = item.nombreCliente
+        const tdIdNumber = document.createElement("td"); 
+        tdIdNumber.innerHTML = item.cedulaCliente
         const tdEmail = document.createElement("td"); 
-        tdEmail.innerHTML = cliente.email
-        const tdJoined = document.createElement("td"); 
-        tdJoined.innerHTML = cliente.joined
-        
-        const tdButtons = document.createElement("td"); 
-
-        const newButtonContainer = document.createElement("div"); 
-        const newButtonEdit = document.createElement("button"); 
-        newButtonEdit.onclick = () => sendQueryId(cliente.id);
-        newButtonEdit.classList.add('btn')
-        newButtonEdit.classList.add('btn-outline-primary')
-        newButtonEdit.classList.add('btn-sm')
-        newButtonEdit.classList.add('m-0')
-        newButtonEdit.classList.add('waves-effect')
-        newButtonEdit.classList.add('mr-2')
-        const iconEdit =  document.createElement("span"); 
-        iconEdit.setAttribute('data-feather','edit')
-        newButtonEdit.appendChild(iconEdit)
-
-        const newButtonRemove = document.createElement("button"); 
-        newButtonRemove.onclick = () => alert(cliente.name);
-        newButtonRemove.classList.add('btn')
-        newButtonRemove.classList.add('btn-outline-danger')
-        newButtonRemove.classList.add('btn-sm')
-        newButtonRemove.classList.add('m-0')
-        newButtonRemove.classList.add('waves-effect')
-        const iconRemove =  document.createElement("span"); 
-        iconRemove.setAttribute('data-feather','trash-2')
-        newButtonRemove.appendChild(iconRemove)
-
-        newButtonContainer.appendChild(newButtonEdit)
-        newButtonContainer.appendChild(newButtonRemove)
-
-        tdButtons.appendChild(newButtonContainer)
+        tdEmail.innerHTML = item.correoCliente
+        const tdProducts = document.createElement("td"); 
+        tdProducts.innerHTML = productos
+        const tdDate = document.createElement("td"); 
+        tdDate.innerHTML = date
 
         newtr.appendChild(tdId);
+        newtr.appendChild(tdDate);
         newtr.appendChild(tdName);
+        newtr.appendChild(tdIdNumber);
         newtr.appendChild(tdEmail);
-        newtr.appendChild(tdJoined);
-        newtr.appendChild(tdButtons);
+        newtr.appendChild(tdProducts);
 
         tbody.appendChild(newtr)
     })
-    feather.replace()
+        feather.replace()
+
+
+        table = $('#dataTable').DataTable({
+            destroy: true,
+            "pagingType": 'first_last_numbers'
+        })
+    }
+
+    
+
 
     stopTableLoading()
 }
 
-$('#dataTable').DataTable({
-    "pagingType": 'first_last_numbers'
-})
+
 
 const sendQueryId = (id) => {
     const url = '/cliente/editar/?id=' + id;
