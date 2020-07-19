89d02fd759fd6819c9b1592931e51c376b9f704d
code.developer13 <dev13@codecraft.com>
2020-07-19 02:07:45 -0400

    -fix navbar

--- inventario/javascript/editar-orden.js
+++ inventario/javascript/editar-orden.js
@@ -6,11 +6,43 @@ const form = document.getElementById('edit-user-form')
 const buttonLoading = document.getElementById('buttonLoading')
 
 
+const fillSelectroduct = async (id,fecha) => {
+    const today = new Date()
+    const day = today.getDate() > 9? today.getDate() : '0'+today.getDate()
+    const month = today.getMonth() +1 > 9? today.getMonth()+1 : '0'+(today.getMonth()+1)
+    const date = today.getFullYear()+'-'+month +'-'+day;
+    
+    fechaDeEntrega.setAttribute('min',date)
+
+    const fechaDeEntregaActual = new Date(fecha)
+    const dayActual = fechaDeEntregaActual.getDate() > 9? fechaDeEntregaActual.getDate() : '0'+fechaDeEntregaActual.getDate()
+    const monthActual = fechaDeEntregaActual.getMonth() +1 > 9? fechaDeEntregaActual.getMonth()+1 : '0'+(fechaDeEntregaActual.getMonth()+1)
+    const dateActual = fechaDeEntregaActual.getFullYear()+'-'+monthActual +'-'+dayActual;
+
+    fechaDeEntrega.value = dateActual
+
+    fetch('http://localhost:3000/product-list')
+        .then(res=>res.json())
+        .then(res=>{
+            res.map(item=>{
+                const optionProduct = document.createElement("option"); 
+                optionProduct.setAttribute('value',item.id)
+                id == item.id && optionProduct.setAttribute('selected','')
+                optionProduct.innerHTML = item.nombre
+
+                selectProductos.appendChild(optionProduct)
+            })
+            
+
+        })
+
+}
+
+
 submitButton.addEventListener('click',(e)=>{
     e.preventDefault()
     if (form.checkValidity() === false) {
         event.stopPropagation();
-        console.log('hola')
     }else{
         const urlParams = new URLSearchParams(window.location.search);
         const id = urlParams.get('id');
@@ -30,6 +62,7 @@ submitButton.addEventListener('click',(e)=>{
                 if(resJson!=='Formulario Llenado Incorrectamente')
                 window.location.replace("/inventario/manejar-ordenes")
             })
+            .catch((err)=>err)
     }
     form.classList.add('was-validated');
 })
@@ -42,10 +75,8 @@ window.addEventListener('load', async () => {
     const res = await fetch('http://localhost:3000/orden-get/'+id)
     const resJson = await res.json()
 
-    console.log(resJson.fechaEntrega)
+    fillSelectroduct(resJson.idProducto,resJson.fechaEntrega)
 
-    selectProductos.value = resJson.selectProductos
-    fechaDeEntrega.value = resJson.fechaEntrega
     cantidad.value = resJson.cantidad
 
     submitButton.toggleAttribute('disabled')
